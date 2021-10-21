f99e7385ee7882ea4dc4b29294a1f735c9eafdb8
Akbar Ghani <87202404+akbarghani@users.noreply.github.com>
2021-10-22 02:12:12 +0530

    Add files via upload
    This one is the third assignment of ineuron.ai

---
+++ ASSIGNMENT 3 INEURON.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ##### Q1
+# 
+# What will the output be of ((500//7) % 5) ** 3?
+
+# In[2]:
+
+
+#The code  is written for the question no 1  and the out put is one 
+x=((500//7) % 5) ** 3
+print(x)
+
+
+# ##### Q2
+# 
+# How will you extract ‘love’ from the string S = “I love Python”? (More than one option may be correct.).
+
+# In[20]:
+
+
+S = "I love Python"
+S1=slice(1,6)
+print(S[S1])
+
+
+# ##### Q3
+# 
+# What will the output of 3 * 3 ** 3 be?
+
+# ##81
+# 
+
+# ##### Q4
+# 
+# If you have a tuple T = (3, 5, 7, 11), what will the output of T.append(9) be?
+
+# In[9]:
+
+
+T = (3, 5, 7, 11)
+T.append(9) 
+##Tuple is not imutable . so we can not append the values in tuple
+
+
+# ##### Q5
+# 
+# What will the output of the following program be?
+# 
+# L1 = ['Vikas', 'Akshay', 'Sanskar', 'Mahima']
+# 
+# print(L1[1][-1])
+
+# In[7]:
+
+
+L1 = ['Vikas', 'Akshay', 'Sanskar', 'Mahima']
+
+print(L1[1][-1])
+
+
+# ##### Q6
+# 
+# What will be the output ?
+# 
+# l = [32, 34, 12, 27, 33]
+# 
+# l.append([14, 19])
+# 
+# print(len(l))
+
+# In[6]:
+
+
+l = [32, 34, 12, 27, 33]
+
+l.append([14, 19])
+
+print(len(l))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
